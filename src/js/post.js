62a18c1992dddc3c14fa9f9ecd95c819fd699ce3
Emilian Mateut <emilian.mateut@gmail.com>
2021-04-28 21:25:18 +0200

    remove font Source Sans Pro from scss file. Make arrows from recomended visible when there are more recommendations than visible

--- src/js/post.js
+++ src/js/post.js
@@ -164,7 +164,7 @@ $(() => {
     })
 
     recommendedSlider.on('length.change', (length) => {
-      if (length <= 3) {
+      if ((isMobile() && length === 1) || (!isMobile() && length <= 3)) {
         recommendedSlider.update({ type: 'slider' })
         $recommendedSlider.find('.js-controls').remove()
       }
